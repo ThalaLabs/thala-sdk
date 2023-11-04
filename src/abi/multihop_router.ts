3e5866dc63a51ee6e6b9a54204349ce734d52c8c
SamuelQZQ <qianzheqi@foxmail.com>
2023-11-04 12:01:24 +0800

    use mainnet address

--- src/abi/multihop_router.ts
+++ src/abi/multihop_router.ts
@@ -1,331 +1,344 @@
-// TODO: deploy router and add ABI
 export const MULTIHOP_ROUTER_ABI = {
-  address: "0xe8643d5170d010105fc2363f59cadd6f069cdf6ce3d6fc0f0cf674194090db27",
-  name: "router",
-  friends: [],
-  exposed_functions: [
+  "name": "router",
+  "address": "0xd54f477c1436849a8c9c04e39f341b8ab81c0f67f4638f63eae229d0d2f1313f",
+  "exposed_functions": [
     {
-      name: "swap_exact_in_2",
-      visibility: "public",
-      is_entry: true,
-      is_view: false,
-      generic_type_params: [
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
+      "name": "swap_exact_in_2",
+      "params": [
+        "&signer",
+        "u64",
+        "u64"
       ],
-      params: ["&signer", "u64", "u64"],
-      return: [],
+      "return": [],
+      "is_view": false,
+      "is_entry": true,
+      "visibility": "public",
+      "generic_type_params": [
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        }
+      ]
     },
     {
-      name: "swap_exact_in_3",
-      visibility: "public",
-      is_entry: true,
-      is_view: false,
-      generic_type_params: [
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
+      "name": "swap_exact_in_3",
+      "params": [
+        "&signer",
+        "u64",
+        "u64"
       ],
-      params: ["&signer", "u64", "u64"],
-      return: [],
+      "return": [],
+      "is_view": false,
+      "is_entry": true,
+      "visibility": "public",
+      "generic_type_params": [
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        }
+      ]
     },
     {
-      name: "swap_exact_out_2",
-      visibility: "public",
-      is_entry: true,
-      is_view: false,
-      generic_type_params: [
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
+      "name": "swap_exact_out_2",
+      "params": [
+        "&signer",
+        "u64",
+        "u64"
       ],
-      params: ["&signer", "u64", "u64"],
-      return: [],
+      "return": [],
+      "is_view": false,
+      "is_entry": true,
+      "visibility": "public",
+      "generic_type_params": [
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        }
+      ]
     },
     {
-      name: "swap_exact_out_3",
-      visibility: "public",
-      is_entry: true,
-      is_view: false,
-      generic_type_params: [
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
-        {
-          constraints: [],
-        },
+      "name": "swap_exact_out_3",
+      "params": [
+        "&signer",
+        "u64",
+        "u64"
       ],
-      params: ["&signer", "u64", "u64"],
-      return: [],
-    },
-  ],
-  structs: [],
+      "return": [],
+      "is_view": false,
+      "is_entry": true,
+      "visibility": "public",
+      "generic_type_params": [
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        },
+        {
+          "constraints": []
+        }
+      ]
+    }
+  ]
 } as const;
